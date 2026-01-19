343884b9972842be0b13cc1c3b0a457bb8f49cf3
IAFahim <iafahim.dev@gmail.com>
2026-01-19 10:28:31 +0600

    Initial commit - DOD architecture refactor - Clean Code Mandates compliance

---
+++ Runtime/StateMachineBehaviourPlayerCombatComponent.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using AV.Unity.Extend.Runtime.Cache;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[HelpURL("https://github.com/IAFahim/AV.StateMachineBehaviour")]
+
+namespace AV.StateMachineBehaviour.Runtime
+{
+    public class StateMachineBehaviourPlayerCombatComponent : UnityEngine.StateMachineBehaviour
+    {
+        private static readonly Dictionary<EntityId, PlayerCombatComponent> Lookup = new();
+        [SerializeField] private string tag = "Attack";
+        private int _targetTagHash;
+
+        private void OnEnable()
+        {
+            _targetTagHash = Animator.StringToHash(tag);
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (stateInfo.tagHash != _targetTagHash) return;
+
+            var id = animator.GetEntityId();
+            if (!animator.TryGetComponentInParentCached(id, Lookup, out var combatComponent)) return;
+            combatComponent.AttackFinished();
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void InitGlobal()
+        {
+            Lookup.Clear();
+
+            SceneManager.activeSceneChanged -= OnSceneChanged;
+            SceneManager.activeSceneChanged += OnSceneChanged;
+        }
+
+        private static void OnSceneChanged(Scene current, Scene next)
+        {
+            Lookup.Clear();
+        }
+    }
+}
